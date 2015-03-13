a2fb73be4b8220ac0f2619d5705b0291932961f3
oscar <oscar@ZEUS>
2015-03-13 13:18:54 +0100

    Nou component -> finestra amb boto

---
+++ ProvesSWING/src/components/FinestraAmbBoto.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package components;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author oscar
+ */
+public class FinestraAmbBoto implements ActionListener{
+    private JFrame finestra;
+    public FinestraAmbBoto(){
+        finestra = new JFrame();
+        finestra.setSize(500,500);
+        finestra.setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
+        JButton btnTancar = new JButton("Tencar");
+        btnTancar.addActionListener(this);
+        finestra.add(btnTancar);
+    }
+    
+    public void mostrar(){
+        finestra.setVisible(true);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        System.exit(0);
+    }
+}
---
+++ ProvesSWING/src/provesswing/ProvesSWING.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package provesswing;
+
+import components.FinestraAmbBoto;
+
+/**
+ *
+ * @author oscar
+ */
+public class ProvesSWING {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        new FinestraAmbBoto().mostrar();
+    }
+    
+}
