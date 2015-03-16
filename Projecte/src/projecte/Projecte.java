799b044fd88540c3682218a4ac7af63b983b5439
Moi <Moi@192.168.20.205>
2015-03-16 10:03:14 +0100

    res de res

--- Projecte/src/projecte/Projecte.java
+++ Projecte/src/projecte/Projecte.java
@@ -17,6 +17,7 @@ public class Projecte {
     public static void main(String[] args) {
         System.out.println("Hello projecte de programacio");
         System.out.println("caca");
+        System.out.println("caca");
     }
     
 }
